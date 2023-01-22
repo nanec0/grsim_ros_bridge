b49cb9af95db6568bf08b9bb91fcc48d737422a6
ricardoGrando <ricardogrando13@gmail.com>
2023-01-22 12:47:29 -0300

    wahtever dude

--- scripts/grsim_ros_bridge.py
+++ scripts/grsim_ros_bridge.py
@@ -52,7 +52,7 @@ def move_robot_yellow(data, id):
         robot_command = RobotCommand(robot_id=id, move_command=move_command, kick_speed=KICKER_SPEED, kick_angle=0, dribbler_speed=0)
     else:
         robot_command = RobotCommand(robot_id=id, move_command=move_command, kick_speed=0, kick_angle=0, dribbler_speed=0)
-        
+
     clientyellow.send_action_command(robot_command)
 
 def cmd_blue_0_callback(data):
@@ -88,16 +88,16 @@ def cmd_yellow_4_callback(data):
 if __name__ == "__main__":
     rospy.init_node("grsim_ros_bridge", anonymous=False)
 
-    rospy.Subscriber("robot_blue_0/cmd_vel", SSL, cmd_blue_0_callback)
-    rospy.Subscriber("robot_blue_1/cmd_vel", SSL, cmd_blue_1_callback)
-    rospy.Subscriber("robot_blue_2/cmd_vel", SSL, cmd_blue_2_callback)
-    rospy.Subscriber("robot_blue_3/cmd_vel", SSL, cmd_blue_3_callback)
-    rospy.Subscriber("robot_blue_4/cmd_vel", SSL, cmd_blue_4_callback)
+    rospy.Subscriber("robot_blue_0/cmd", SSL, cmd_blue_0_callback)
+    rospy.Subscriber("robot_blue_1/cmd", SSL, cmd_blue_1_callback)
+    rospy.Subscriber("robot_blue_2/cmd", SSL, cmd_blue_2_callback)
+    rospy.Subscriber("robot_blue_3/cmd", SSL, cmd_blue_3_callback)
+    rospy.Subscriber("robot_blue_4/cmd", SSL, cmd_blue_4_callback)
 
-    rospy.Subscriber("robot_yellow_0/cmd_vel", SSL, cmd_yellow_0_callback)
-    rospy.Subscriber("robot_yellow_1/cmd_vel", SSL, cmd_yellow_1_callback)
-    rospy.Subscriber("robot_yellow_2/cmd_vel", SSL, cmd_yellow_2_callback)
-    rospy.Subscriber("robot_yellow_3/cmd_vel", SSL, cmd_yellow_3_callback)
-    rospy.Subscriber("robot_yellow_4/cmd_vel", SSL, cmd_yellow_4_callback)
+    rospy.Subscriber("robot_yellow_0/cmd", SSL, cmd_yellow_0_callback)
+    rospy.Subscriber("robot_yellow_1/cmd", SSL, cmd_yellow_1_callback)
+    rospy.Subscriber("robot_yellow_2/cmd", SSL, cmd_yellow_2_callback)
+    rospy.Subscriber("robot_yellow_3/cmd", SSL, cmd_yellow_3_callback)
+    rospy.Subscriber("robot_yellow_4/cmd", SSL, cmd_yellow_4_callback)
 
     rospy.spin()
